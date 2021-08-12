2668fad74d195a4918816151fc070c5c2e31ea87
kunljha <jha711kunal@gmail.com>
2021-08-12 22:17:41 +0530

    es5 prototype syntax used

---
+++ js/hangman.js
@@ -0,0 +1,62 @@
+class Hangman {
+	constructor(word, count) {
+		this.word = word.toLowerCase().split('')
+		this.guessCount = count
+		this.guessedLetters = []
+		this.status = 'playing'
+	}
+	get puzzle() {
+		let puzzle = ''
+		this.word.forEach((letter) => {
+			if (this.guessedLetters.includes(letter) || letter === ' ') {
+				puzzle += letter
+			} else {
+				puzzle += '*'
+			}
+		})
+
+		return puzzle
+	}
+	makeGuess(guess) {
+		guess = guess.toLowerCase()
+		const isUnique = !this.guessedLetters.includes(guess)
+		const isWrongGuess = !this.word.includes(guess)
+
+		if (this.status !== 'playing') {
+			return
+		}
+
+		if (isUnique) {
+			this.guessedLetters.push(guess)
+		}
+
+		if (isUnique && isWrongGuess) {
+			this.guessCount--
+		}
+
+		this.gameStatus()
+	}
+	gameStatus() {
+		const finished = this.word.every(
+			(letter) => this.guessedLetters.includes(letter) || letter === ' '
+		)
+
+		if (this.guessCount < 0) {
+			this.status = 'failed'
+		} else if (finished) {
+			this.status = 'finished'
+		} else {
+			this.status = 'playing'
+		}
+	}
+	get statusMessage() {
+		if (this.status === 'playing') {
+			return `Incorrect guesses allowed: ${this.guessCount}`
+		} else if (this.status === 'finished') {
+			return 'Great work! You guessed the word correctly.'
+		} else {
+			return `Nice try! But you ran out-of guesses.
+		The word was "${this.word.join('')}"`
+		}
+	}
+}
---
+++ js/requests.js
@@ -0,0 +1,11 @@
+const getPuzzle = async (wordCount) => {
+	const response = await fetch(
+		`//puzzle.mead.io/puzzle?wordCount=${wordCount}`
+	);
+	if (response.status === 200) {
+		const data = await response.json();
+		return data.puzzle;
+	} else {
+		throw new Error('Something went wrong!');
+	}
+};
